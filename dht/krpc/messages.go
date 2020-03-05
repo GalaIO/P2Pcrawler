dbb81017a8b3942addc94691869648212fb3702c
gyj176383 <gyj176383@alibaba-inc.com>
2020-03-05 16:47:00 +0800

    增加消息抽象

--- dht/krpc/messages.go
+++ dht/krpc/messages.go
@@ -1,18 +1,165 @@
 package krpc
 
-import "github.com/GalaIO/P2Pcrawler/dht"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/GalaIO/P2Pcrawler/dht"
+)
 
-// define query msg
-func withPingMsg(txId string, nodeId string) dht.Dict {
-	return withQueryMsg(txId, "ping", dht.Dict{
-		"id": nodeId,
-	})
+type Response interface {
+	TxId() string
+	NodeId() string
+	Body() dht.Dict
+	Error() bool
+	RawData() dht.Dict
+	fmt.Stringer
 }
 
-func withFindNodeMsg(txId string, nodeId, target string) dht.Dict {
+type BaseResponse struct {
+	txId  string
+	qType string
+	body  dht.Dict
+	isErr bool
+}
 
-	return withQueryMsg(txId, "find_node", dht.Dict{
+func NewBaseResponse(txId, qType string, isErr bool, body dht.Dict) Response {
+	return &BaseResponse{
+		txId:  txId,
+		qType: qType,
+		body:  body,
+		isErr: isErr,
+	}
+}
+
+func (b *BaseResponse) TxId() string {
+	return b.txId
+}
+
+func (b *BaseResponse) NodeId() string {
+	return b.body.GetString("id")
+}
+
+func (b *BaseResponse) Body() dht.Dict {
+	return b.body
+}
+
+func (b *BaseResponse) Error() bool {
+	return b.isErr
+}
+
+func (b *BaseResponse) RawData() dht.Dict {
+	respType := "r"
+	if b.isErr {
+		respType = "e"
+	}
+	return dht.Dict{
+		"t": b.txId,
+		"y": respType,
+		"r": b.body,
+	}
+}
+
+func (b *BaseResponse) String() string {
+	bytes, err := json.Marshal(b.RawData())
+	if err != nil {
+		return err.Error()
+	}
+
+	return string(bytes)
+}
+
+type RespHandlerFunc func(req Request, resp Response)
+
+type Request interface {
+	TxId() string
+	Type() string
+	Body() dht.Dict
+	Handler() RespHandlerFunc
+	RawData() dht.Dict
+	fmt.Stringer
+}
+
+type BaseRequest struct {
+	txId    string
+	qType   string
+	body    dht.Dict
+	handler RespHandlerFunc
+}
+
+func NewBaseRequest(txId, qtype string, body dht.Dict, handlerFunc RespHandlerFunc) Request {
+	return &BaseRequest{
+		txId:    txId,
+		qType:   qtype,
+		body:    body,
+		handler: handlerFunc,
+	}
+}
+
+func (b *BaseRequest) TxId() string {
+	return b.txId
+}
+
+func (b *BaseRequest) Type() string {
+	return b.qType
+}
+
+func (b *BaseRequest) Body() dht.Dict {
+	return b.body
+}
+
+func (b *BaseRequest) Handler() RespHandlerFunc {
+	return b.handler
+}
+
+func (b *BaseRequest) RawData() dht.Dict {
+	return dht.Dict{
+		"t": b.txId,
+		"y": "q",
+		"q": b.qType,
+		"a": b.body,
+	}
+}
+
+func (b *BaseRequest) String() string {
+	bytes, err := json.Marshal(b)
+	if err != nil {
+		return err.Error()
+	}
+
+	return string(bytes)
+}
+
+// response
+func withResponse(txId string, resp dht.Dict) dht.Dict {
+	return dht.Dict{
+		"t": txId,
+		"y": "r",
+		"r": resp,
+	}
+}
+
+// error
+func withParamErr(msg string) dht.Dict {
+	if len(msg) <= 0 {
+		msg = "invalid param"
+	}
+	return withErr(dht.ProtocolErr, msg)
+}
+
+func withErr(code dht.DhtErrCode, errMsg string) dht.Dict {
+	return dht.Dict{"t": "aa", "y": "e", "e": dht.List{code, errMsg}}
+}
+
+// define query msg
+func withPingMsg(txId string, nodeId string, handler RespHandlerFunc) Request {
+	return NewBaseRequest(txId, "ping", dht.Dict{
+		"id": nodeId,
+	}, handler)
+}
+
+func withFindNodeMsg(txId string, nodeId, target string, handler RespHandlerFunc) Request {
+	return NewBaseRequest(txId, "find_node", dht.Dict{
 		"id":     nodeId,
 		"target": target,
-	})
+	}, handler)
 }
--- dht/krpc/messages_test.go
+++ dht/krpc/messages_test.go
@@ -8,12 +8,16 @@ import (
 
 func TestGenQueryMsg(t *testing.T) {
 
-	var msg dht.Dict
+	var req Request
 
-	msg = withPingMsg("aa", testNodeId)
-	assert.Equal(t, dht.Dict{"t": "aa", "y": "q", "q": "ping", "a": dht.Dict{"id": "abcdefghij0123456789"}}, msg)
+	req = withPingMsg("aa", testNodeId, func(req Request, resp Response) {
 
-	msg = withFindNodeMsg("aa", "abcdefghij0123456789", "mnopqrstuvwxyz123456")
-	assert.Equal(t, dht.Dict{"t": "aa", "y": "q", "q": "find_node", "a": dht.Dict{"id": "abcdefghij0123456789", "target": "mnopqrstuvwxyz123456"}}, msg)
+	})
+	assert.Equal(t, dht.Dict{"t": "aa", "y": "q", "q": "ping", "a": dht.Dict{"id": "abcdefghij0123456789"}}, req.RawData())
+
+	req = withFindNodeMsg("aa", "abcdefghij0123456789", "mnopqrstuvwxyz123456", func(req Request, resp Response) {
+
+	})
+	assert.Equal(t, dht.Dict{"t": "aa", "y": "q", "q": "find_node", "a": dht.Dict{"id": "abcdefghij0123456789", "target": "mnopqrstuvwxyz123456"}}, req.RawData())
 
 }
